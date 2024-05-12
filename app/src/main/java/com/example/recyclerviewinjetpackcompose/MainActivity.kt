e1a52d5c9bb98837e4a80e52d4f31c75055e6e1d
Mardia <maridiaakter@gmail.com>
2024-05-12 21:22:49 +0600

    updated code

--- app/src/main/java/com/example/recyclerviewinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewinjetpackcompose/MainActivity.kt
@@ -4,53 +4,105 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.recyclerviewinjetpackcompose.ui.theme.RecyclerViewInJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserCard()
+            UserList(users)
         }
     }
 }
 
+data class User(
+    val id: Int
+)
+
 @Composable
-fun UserCard() {
+fun MainContent(){
+    /*val user = User(1)
+    val users = remember { mutableStateOf(user)*//* }*/
+
+    Box{
+        UserList(users = users)
+        Button(onClick = {
+        },modifier = Modifier.align(Alignment.BottomCenter)) {
+            Text(text = "Add More")
+        }
+    }
+}
+
+val users = mutableListOf(
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+)
+@Composable
+fun UserList(users: List<User> = listOf()){
+    val name = remember { mutableStateOf("Mardia")}
+
+    LazyColumn{//for RecyclerView
+        items(users){user ->
+            UserCard()
+        }
+    }
+}
+@Composable
+fun UserCard(){
     Card(
-        elevation = 4.dp,
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
         modifier = Modifier
             .padding(12.dp)
             .fillMaxWidth()
             .wrapContentHeight()
-    ) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(12.dp)
-            .padding(12.dp)
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(12.dp)
+                .padding(12.dp)
+
         ){
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                painter = painterResource(id = R.drawable.ic_launcher_background),
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -58,19 +110,22 @@ fun UserCard() {
                     .clip(CircleShape)
             )
             Column {
-
+                Text(text = stringResource(id = R.string.dummy_text),
+                    modifier = Modifier.padding(8.dp), color = Color.Black
+                )
+                Button(onClick = {
+                    //perform button click action here
+                }) {
+                    Text(text = "View Profile")
+                }
             }
-
         }
-        
     }
 }
-
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    RecyclerViewInJetpackComposeTheme {
-        UserCard()
+fun DefaultPreview() {
+    Column {
+        MainContent()
     }
 }
